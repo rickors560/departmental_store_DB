63229a0774dd5e7d9ebb186a607bc1046dc8ed87
Ritik <ritik.sharma@taazaa.com>
2021-05-17 17:43:42 +0530

    Initial Commit

--- departmental_store_data.sql
+++ departmental_store_data.sql
@@ -1,3 +1,19 @@
+-- Database: departmental_store_data
+
+-- DROP DATABASE departmental_store_data;
+
+CREATE DATABASE departmental_store_data
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+
+
 -- Creating table staff
 CREATE TABLE staff(
 	staff_id SERIAL PRIMARY KEY,
